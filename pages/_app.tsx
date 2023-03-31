db821cc33ad60855b441712d63817e97b5478dfe
Angelo Berrios <angelo.berriosgrafit@gmail.com>
2023-03-31 16:44:25 -0300

    [Add] pay with paypal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,26 +6,33 @@ import { CartProvider, UiProvider } from '../context'
 import { SWRConfig } from 'swr'
 import { SessionProvider } from 'next-auth/react'
 import { AuthProvider } from '../context/auth'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider>
-      <AuthProvider>
-        <SWRConfig
-          value={{
-            //refreshInterval: 3000,
-            fetcher: (resource, init) =>
-              fetch(resource, init).then((res) => res.json()),
-          }}
-        >
-          <CartProvider>
-            <UiProvider>
-              <ThemeProvider theme={lightTheme}>
-                <Component {...pageProps} />
-              </ThemeProvider>
-            </UiProvider>
-          </CartProvider>
-        </SWRConfig>
-      </AuthProvider>
+      <PayPalScriptProvider
+        options={{
+          'client-id': process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || '',
+        }}
+      >
+        <AuthProvider>
+          <SWRConfig
+            value={{
+              //refreshInterval: 3000,
+              fetcher: (resource, init) =>
+                fetch(resource, init).then((res) => res.json()),
+            }}
+          >
+            <CartProvider>
+              <UiProvider>
+                <ThemeProvider theme={lightTheme}>
+                  <Component {...pageProps} />
+                </ThemeProvider>
+              </UiProvider>
+            </CartProvider>
+          </SWRConfig>
+        </AuthProvider>
+      </PayPalScriptProvider>
     </SessionProvider>
   )
 }
--- pages/orders/[id].tsx
+++ pages/orders/[id].tsx
@@ -1,13 +1,25 @@
-import { Box, Typography, Link, Chip } from '@mui/material'
+import {
+  Box,
+  Typography,
+  Link,
+  Chip,
+  Grid,
+  Card,
+  CircularProgress,
+} from '@mui/material'
 import { ShopLayout } from '../../components/layouts'
 import { useRouter } from 'next/router'
 import { GetServerSideProps, NextPage } from 'next'
 import { getSession } from 'next-auth/react'
 import { dbOrders } from '../../database'
-import { IOrder } from '../../interfaces'
+import { IOrder, IPaypal } from '../../interfaces'
 import { zones } from '../../utils/zones'
 import { CartList } from '../../components/cart'
 import { Cashformat } from '../../utils'
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js'
+import { useState } from 'react'
+import axios from 'axios'
+import { CreditCardOffOutlined, CreditScoreOutlined } from '@mui/icons-material'
 
 interface Props {
   order: IOrder
@@ -19,8 +31,37 @@ const zoneDef = (zone: String) => {
 }
 
 const Order: NextPage<Props> = ({ order }) => {
+  const [isPaying, setIsPaying] = useState(false)
+
   const router = useRouter()
+
   const { shippingAddress } = order
+
+  const onOrderCompleted = async (details: IPaypal.OrderResponseBody) => {
+    if (details.status !== 'COMPLETED') {
+      return alert('No hay pago en Paypal')
+    }
+
+    setIsPaying(true)
+
+    try {
+      const { data } = await axios
+        .create({
+          baseURL: '/api',
+        })
+        .post(`/orders/pay`, {
+          transactionId: details.id,
+          orderId: order._id,
+        })
+
+      router.reload()
+    } catch (error) {
+      setIsPaying(false)
+      console.log(error)
+      alert('Error')
+    }
+  }
+
   return (
     <ShopLayout
       title={'Orden | Cupcakes'}
@@ -63,53 +104,127 @@ const Order: NextPage<Props> = ({ order }) => {
           </Box>
         </Box>
 
-        <Box
-          m={'auto'}
-          sx={{
-            backgroundColor: '#4359',
-            color: '#fff',
-            p: '10px',
-            borderRadius: '10px',
-            mt: '40px',
-          }}
-        >
-          <Typography variant="h3" component="h3" fontSize={20}>
-            Sector: {zoneDef(shippingAddress.zone)}
-          </Typography>
-          <Typography variant="h3" component="h3" fontSize={20}>
-            Dirección: {shippingAddress.address}
-          </Typography>
-          <Typography variant="h3" component="h3" fontSize={20}>
-            Entrega a: {shippingAddress.firstName} {shippingAddress.lastName}
-          </Typography>
-        </Box>
+        <Grid container justifyContent={'center'}>
+          <Grid item sm={8} xs={12}>
+            <Box sx={{ padding: '10px', mt: '40px' }}>
+              <CartList products={order.orderItems} />
+            </Box>
+          </Grid>
 
-        <Box sx={{ maxWidth: '70%', padding: '10px', mt: '40px' }}>
-          <CartList products={order.orderItems} />
-        </Box>
+          <Grid item sm={4} xs={12} p={2}>
+            <Card className="summary-card">
+              <Box
+                m={'auto'}
+                sx={{
+                  backgroundColor: '#4359',
+                  color: '#fff',
+                  p: '10px',
+                  borderRadius: '10px',
+                  mt: '40px',
+                  width: '90%',
+                }}
+              >
+                <Typography variant="h3" component="h3" fontSize={20}>
+                  Sector: {zoneDef(shippingAddress.zone)}
+                </Typography>
+                <Typography variant="h3" component="h3" fontSize={20}>
+                  Dirección: {shippingAddress.address}
+                </Typography>
+                <Typography variant="h3" component="h3" fontSize={20}>
+                  Entrega a: {shippingAddress.firstName}{' '}
+                  {shippingAddress.lastName}
+                </Typography>
+              </Box>
 
-        <Box
-          m={'auto'}
-          sx={{
-            backgroundColor: '#4359',
-            color: '#fff',
-            p: '10px',
-            borderRadius: '10px',
-          }}
-        >
-          <Typography variant="h3" component="h3" fontSize={20}>
-            Cantidad productos: {order.numberOfItems}
-          </Typography>
-          <Typography variant="h3" component="h3" fontSize={20}>
-            Subtotal: {Cashformat(order.subTotal, 0)}
-          </Typography>
-          <Typography variant="h3" component="h3" fontSize={20}>
-            Impuesto: {order.tax}
-          </Typography>
-          <Typography variant="h3" component="h3" fontSize={20}>
-            Total: {Cashformat(order.total, 2)}
-          </Typography>
-        </Box>
+              <Box
+                m={'auto'}
+                sx={{
+                  backgroundColor: '#4359',
+                  color: '#fff',
+                  p: '10px',
+                  borderRadius: '10px',
+                  mt: '40px',
+                  width: '90%',
+                }}
+              >
+                <Typography variant="h3" component="h3" fontSize={20}>
+                  Cantidad productos: {order.numberOfItems}
+                </Typography>
+                <Typography variant="h3" component="h3" fontSize={20}>
+                  Subtotal: {Cashformat(order.subTotal, 0)}
+                </Typography>
+                <Typography variant="h3" component="h3" fontSize={20}>
+                  Impuesto: {order.tax}
+                </Typography>
+                <Typography variant="h3" component="h3" fontSize={20}>
+                  Total: {Cashformat(order.total, 2)}
+                </Typography>
+              </Box>
+
+              <Box
+                sx={{ mt: 3 }}
+                display={`${order.isPaid ? 'flex' : 'none'}`}
+                flexDirection="column"
+                textAlign={'center'}
+              >
+                <Chip
+                  color="success"
+                  label="Orden pagada"
+                  sx={{ width: '70%', m: 'auto', mb: '15px' }}
+                />
+              </Box>
+
+              <Box
+                sx={{ mt: 3 }}
+                display={`${!order.isPaid ? 'flex' : 'none'}`}
+                flexDirection="column"
+              >
+                {
+                  <Box
+                    display={!isPaying ? 'none' : 'flex'}
+                    sx={{
+                      m: 'auto',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <Typography>
+                      Cargando... <CircularProgress />
+                    </Typography>
+                  </Box>
+                }
+
+                <Box
+                  sx={{
+                    display: isPaying ? 'none' : 'flex',
+                    flex: 1,
+                    flexDirection: 'column',
+                  }}
+                >
+                  <PayPalButtons
+                    createOrder={(data, actions) => {
+                      return actions.order.create({
+                        purchase_units: [
+                          {
+                            amount: {
+                              value: order.total.toString(),
+                            },
+                          },
+                        ],
+                      })
+                    }}
+                    onApprove={(data, actions) => {
+                      return actions.order!.capture().then((details) => {
+                        onOrderCompleted(details)
+                        const name = details.payer.name!.given_name
+                        alert(`Transacción completada por ${name}`)
+                      })
+                    }}
+                  />
+                </Box>
+              </Box>
+            </Card>
+          </Grid>
+        </Grid>
       </Box>
     </ShopLayout>
   )
