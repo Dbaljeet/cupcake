c57fa5a929af26f078d2789018542d3f040e9d44
Angelo Berrios <angelo.berriosgrafit@gmail.com>
2023-04-18 13:10:41 -0400

    [Fix] trying fix 3-504

--- pages/register2.tsx
+++ pages/register2.tsx
@@ -1,15 +1,7 @@
-import { Box } from '@mui/material'
-import { ShopLayout } from '../components/layouts'
 import { getSession } from 'next-auth/react'
 import { getProviders } from 'next-auth/react'
 import { signIn } from 'next-auth/react'
 import { GetServerSideProps } from 'next/types'
-import { useContext } from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { useRouter } from 'next/router'
-import { authContext } from '../context/auth'
 
 type FormData = {
   name: string
@@ -17,54 +9,8 @@ type FormData = {
   password: string
 }
 
-function Register() {
-  const router = useRouter()
-  //await signIn('credentials', {email, password})
-  const [showError, setShowError] = useState(false)
-  const [errorMessage, setErrorMessage] = useState('')
-  const [providers, setProviders] = useState<any>({})
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>()
-
-  const { registerUser } = useContext(authContext)
-
-  useEffect(() => {
-    getProviders().then((prov) => setProviders(prov))
-  }, [])
-
-  const onRegisterForm = async ({ name, email, password }: FormData) => {
-    setShowError(false)
-    const { hasError, message } = await registerUser(name, email, password)
-
-    if (hasError) {
-      setShowError(true)
-      setErrorMessage(message!)
-      setTimeout(() => setShowError(false), 3000)
-      return
-    }
-
-    await signIn('credentials', { email, password })
-  }
-
-  return (
-    <ShopLayout
-      title={'Regístrate'}
-      pageDescription={
-        'Regístrate para comprar los mejores cupcakes de la cuarta región'
-      }
-    >
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        sx={{ minHeight: 'calc(100vh - 200px)' }}
-      ></Box>
-    </ShopLayout>
-  )
+function Register2() {
+  return <>test</>
 }
 
 export const getServerSideProps: GetServerSideProps = async ({
@@ -87,4 +33,4 @@ export const getServerSideProps: GetServerSideProps = async ({
     props: {},
   }
 }
-export default Register
+export default Register2
