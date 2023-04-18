3b41749de56796af91a091edaef188500efb124c
Angelo Berrios <angelo.berriosgrafit@gmail.com>
2023-04-18 13:14:46 -0400

    [Fix] trying fix 4-504

--- pages/register2.tsx
+++ pages/register2.tsx
@@ -2,6 +2,11 @@ import { getSession } from 'next-auth/react'
 import { getProviders } from 'next-auth/react'
 import { signIn } from 'next-auth/react'
 import { GetServerSideProps } from 'next/types'
+import { useContext } from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { authContext } from '../context/auth'
 
 type FormData = {
   name: string
@@ -9,8 +14,39 @@ type FormData = {
   password: string
 }
 
-function Register2() {
-  return <>test</>
+function Register() {
+  //await signIn('credentials', {email, password})
+  const [showError, setShowError] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+  const [providers, setProviders] = useState<any>({})
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>()
+
+  const { registerUser } = useContext(authContext)
+
+  useEffect(() => {
+    getProviders().then((prov) => setProviders(prov))
+  }, [])
+
+  const onRegisterForm = async ({ name, email, password }: FormData) => {
+    setShowError(false)
+    const { hasError, message } = await registerUser(name, email, password)
+
+    if (hasError) {
+      setShowError(true)
+      setErrorMessage(message!)
+      setTimeout(() => setShowError(false), 3000)
+      return
+    }
+
+    await signIn('credentials', { email, password })
+  }
+
+  return <></>
 }
 
 export const getServerSideProps: GetServerSideProps = async ({
@@ -33,4 +69,4 @@ export const getServerSideProps: GetServerSideProps = async ({
     props: {},
   }
 }
-export default Register2
+export default Register
